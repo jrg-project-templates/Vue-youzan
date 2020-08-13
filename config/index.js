7a00928e53874e717a549e1ad0f794a0297d28e5
Big7lion <biglion77@outlook.com>
2020-08-14 02:06:14 +0800

    build

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://jrg-project-templates.github.io/Vue-youzan/dist',
 
     /**
      * Source Maps
